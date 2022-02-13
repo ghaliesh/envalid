eae3f09dfcfcbfbe496b6a47324e4fee5ab8ed17
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2022-02-13 11:51:56 +0200

    feat: add .env file reader

---
+++ file/file.go
@@ -0,0 +1,74 @@
+package envfilereader
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	linesep = "\n"
+	pairsep = "="
+)
+
+type Reader interface {
+	ReadEnvFile(path string) string
+}
+
+type EnvFileReader struct{}
+
+type EnvKeyValuePairs = map[string]string
+
+func (ef *EnvFileReader) getFile(path string) []byte {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic("Can't read file in path")
+	}
+
+	return file
+}
+
+func (ef *EnvFileReader) stringify(file []byte) string {
+	stringified := string(file)
+	if len(stringified) == 0 {
+		panic("File is either empty or invalid")
+	}
+
+	return stringified
+}
+
+func (ef *EnvFileReader) getKeypair(pair string) (key string, value string) {
+	keyval := strings.Split(pair, pairsep)
+	len := len(keyval)
+
+	// making sure the format is always [key, value]
+	if len == 1 {
+		keyval = []string{keyval[0], ""}
+	} else if len > 2 {
+		keyval = []string{keyval[0], strings.Replace(pair, keyval[0], "", 1)}
+	} else {
+		return "", ""
+	}
+
+	key = keyval[0]
+	value = keyval[1]
+	return key, value
+}
+
+func (ef *EnvFileReader) ReadEnvFile(path string) EnvKeyValuePairs {
+	file := ef.getFile(path)
+	str := ef.stringify(file)
+
+	pairs := strings.Split(str, linesep)
+	if len(pairs) == 0 {
+		panic(".env file is invalid")
+	}
+
+	var result = make(EnvKeyValuePairs)
+
+	for _, p := range pairs {
+		key, value := ef.getKeypair(p)
+		result[key] = value
+	}
+
+	return result
+}
