841f0a24544d595991eba59d5c98e753ea2ace11
Ghazwan Aliesh <ghazwan.aliesh@redacreltd.com>
2022-03-16 21:47:56 +0200

    test: increaste envalid test coverage

--- envalid.go
+++ envalid.go
@@ -1,7 +1,10 @@
 package envalid
 
 import (
+	"log"
+
 	"github.com/ghaliesh/envalid/envalidator"
+	"github.com/ghaliesh/envalid/utils"
 )
 
 type EnvalidInterface interface {
@@ -11,11 +14,25 @@ type EnvalidInterface interface {
 type OnErrorHandler = func(err error)
 
 type Envalid struct {
-	OnError OnErrorHandler
+	OnError   OnErrorHandler
+	validator interface{}
+	path      string
 }
 
-func (e *Envalid) ValidateEnv(validator interface{}, path string) {
-	err := envalidator.Validate(validator, path)
+func (e *Envalid) ValidateEnv() {
+	if e.OnError == nil {
+		log.Panic(utils.ErrOnErrorFunctionIsMissing)
+	}
+
+	if len(e.path) == 0 {
+		e.OnError(utils.ErrPathIsNotProvided)
+	}
+
+	if e.validator == nil {
+		e.OnError(utils.ErrValidatorIsNotProvided)
+	}
+
+	err := envalidator.Validate(e.validator, e.path)
 
 	if err != nil {
 		e.OnError(err)
--- utils/errors.go
+++ utils/errors.go
@@ -5,13 +5,19 @@ import (
 	"fmt"
 )
 
-var ErrInvalidEnvFile = errors.New("env file is either empty or invalid")
-var ErrUnhandledTypes = errors.New("unknown types, only primitive types are allowed")
+// user error
+var ErrOnErrorFunctionIsMissing = errors.New("envalid: onError is required")
+var ErrValidatorIsNotProvided = errors.New("envalid: validator is required")
+var ErrPathIsNotProvided = errors.New("envalid: path is required")
+var ErrInvalidEnvFile = errors.New("envalid: env file is either empty or invalid")
+
+// internal error
+var ErrUnhandledTypes = errors.New("envalid: unknown types, only primitive types are allowed")
 
 func KeyDoesNotExistsError(key string) error {
-	return fmt.Errorf("%s is missing from .env file", key)
+	return fmt.Errorf("envalid: %s is missing from .env file", key)
 }
 
 func KeyIsNotOfRightTypeError(key string, val string, typeof string) error {
-	return fmt.Errorf("\n expected value of '%s' to be type of '%s' \n got value '%s'", key, typeof, val)
+	return fmt.Errorf("envalid: \n expected value of '%s' to be type of '%s' \n got value '%s'", key, typeof, val)
 }
